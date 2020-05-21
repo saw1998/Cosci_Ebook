9cd79bf08a4e651c7274a15f1a905edefc1141a6
Sachin Saw <sachinsaw1998@gmail.com>
2020-05-21 11:13:52 +0530

    Main Project Structure

---
+++ app/src/main/java/com/example/cosic_ebook/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cosic_ebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
